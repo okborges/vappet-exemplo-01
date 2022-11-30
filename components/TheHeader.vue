06311e6f3136eb46f0d7a436fd6e262ad7ca9e0d
okborges <rafaelborges.av@gmail.com>
2022-11-30 09:06:36 -0300

    primeiro commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>logo</h1>
+    <h2 class="text-3xl font-bold underline text-red-700">Hello world!</h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+h1 {
+  color: aqua;
+}
+</style>
